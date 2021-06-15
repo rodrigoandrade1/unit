7af2452761ace3e4abda45034b6209de839104ef
Rodrigo Andrade <rodriigosantos11@gmail.com>
2021-06-15 11:55:10 +0000

    start

---
+++ u2_atv01.py
@@ -0,0 +1,31 @@
+# A lanchonete EGGS Lanches iniciou suas atividades na cidade de Aracaju-SE no mês de agosto de 2000 e tem como objetivo principal
+# oferecer produtos de qualidade e atender as expectativas de seus clientes. Para atingir um maior número de clientes,
+# o proprietário da EGGS Lanches solicita a você o desenvolvimento de um programa em Python para gerenciar suas vendas delivery.
+
+# O cardápio da lanchonete EGGS Lanches é o seguinte:
+
+# ESPECIFICAÇÃO CÓDIGO PREÇO
+# Cachorro Quente  100 R$ 4,20
+# Bauru Simples    101 R$ 7,30
+# Bauru com ovo    102 R$ 8,50
+# Hambúrguer       103 R$ 9,20
+# Cheeseburguer    104 R$ 10,30
+# Refrigerante     105 R$ 8,00
+
+# Para cada atendimento realizado por telefone, o programa deverá permitir que o atendente registre o CPF do cliente,
+# o código dos itens do pedido, as quantidades desejadas de cada item e o valor do frete de acordo com a localidade do cliente.
+# O pedido será finalizado quando o atendente informar o código do item do pedido com o valor -1. 
+# O valor do frete não deve ser computado como lucro apenas para o proprietário da lanchonete, mas
+# deve ser considerado que 60% do valor arrecadado com o frete é destinado ao motoboy.
+
+# No final do expediente, o atendente realizará o fechamento do caixa registrando o CPF do cliente com o valor -1.
+# Quando o caixa for fechado, o programa deverá:
+
+# a) Imprimir o valor pago pelo cliente com o pedido mais caro do expediente;
+# b) Imprimir o valor pago pelo cliente com o pedido mais barato do expediente;
+# c) Imprimir o valor arrecadado pelo motoboy no final do expediente;
+# d) Imprimir o valor total arrecadado pelo proprietário do EGGS Lanches no final do expediente.
+
+# aa
+
+valor = []
