1955ef73e7f97257760334caade155b0b8d0f17b
Rodrigo Andrade <rodriigosantos11@gmail.com>
2021-06-19 11:30:13 -1200

    resolução aps unidade 1

---
+++ un1_prova.py
@@ -0,0 +1,22 @@
+# Recentemente ocorreram mudanças na legislação de trânsito referente ao acúmulo de pontos na CNH.
+#  A partir de agora, há uma gradação no aumento desta pontuação. Ou seja, dependendo da gravidade da infração, 
+# ,o condutor pode perder o documento com 20, 30 ou 40 pontos acumulados dentro de 12 meses.
+
+# Se houver duas ou mais infrações gravíssimas dentro desse período, a CNH é suspensa com 20 pontos.
+#  Se houver uma infração gravíssima, a suspensão ocorre com 30 pontos. No entanto, ele só chegará aos 40 pontos
+#  dentro de 12 meses, caso não ocorra nenhuma infração gravíssima.
+
+# O Detran solicitou à equipe de TI um programa em PYTHON para controlar a aplicação dessas novas regras.
+#  Dessa forma, o programa deve solicitar o número da CNH do condutor, nome, sexo, a quantidade de pontos 
+# registrados e a quantidade de infrações gravíssimas. 
+
+# O programa deve solicitar as informações até que o usuário indique que não há mais registros. 
+# Em seguida, o programa deverá gerar as seguintes informações:
+
+# a) A quantidade de condutores do sexo feminino que não tiveram a CNH suspensa; (1,0)
+# b) A média da quantidade de pontos registrados de condutores do sexo feminino; (1,0)
+# c) O percentual de condutores do sexo masculino que tiveram a CNH suspensa com 40 pontos;
+# (1,0)
+# d) A quantidade de condutores que não tiveram CNH suspensa; (1,0)
+# e) O nome e sexo do condutor que teve a maior quantidade de pontos registrados; (1,0)
+# f) O número da CNH do condutor que teve menor pontuação registrada. (1,0)
